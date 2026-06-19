398a9482ed02bb58dcf12a4c80d693d55eb98f6f
Your Name <you@example.com>
2026-06-18 18:54:05 -0600

    Regiter in admin review

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Service, DocumentType, ServiceRequirement, Report, Document, Media, Tramite, AuditLog
+from .models import User, Service, DocumentType, ServiceRequirement, Report, Document, Media, Tramite, AuditLog, Review
 
 
 @admin.register(User)
@@ -23,4 +23,5 @@ admin.site.register(Report)
 admin.site.register(Document)
 admin.site.register(Media)
 admin.site.register(Tramite)
+admin.site.register(Review)
 admin.site.register(AuditLog)
